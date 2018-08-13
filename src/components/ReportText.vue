765cd2daa352332a1f3fbfeaf36bc73c49ac88aa
Dylan <dylan@vandenbosch.co.za>
2018-08-13 13:26:05 +0200

    minor tweaks before store refactor

--- src/components/ReportText.vue
+++ src/components/ReportText.vue
@@ -1,16 +1,23 @@
 <template>
     <v-layout row wrap>
         <v-container grid-list-sx>
-            <v-card>
-                <v-flex xs12>
-                    <h1>The Fair Food Foundation</h1>
-                    <h3>{{ date }}</h3>
-                    <br>
-                    <p>In {{ date }}, approximately {{ seedlingSum }} seedlings were distributed to {{ supportedGrowers }} supported growers, as well as 2200 [DeeNote: this number not is non-calc, dunno where to pull from] to the Mariannhill Co-Op Gardens. This seedling sale figure equates to an approximated planted area of {{ plantedArea }} m<span>2</span>, which if all are successfully grown represent an estimated yield  of approximately {{ cropYield }}T, which at retail value is about R{{ cropValue }} 000.  
-</p>
-                </v-flex>           
-                
-            </v-card>
+            <v-flex xs12 md10 offset-md1>
+                <v-card>
+                    <v-card-title primary-title>
+                        <h1>The Fair Food Foundation</h1>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-flex xs12>
+                            <h3>{{ date }}</h3>
+                            <br>
+                            <h4>Seedling Supply</h4>
+                            <p>In {{ date }}, approximately {{ seedlingSum }} seedlings were distributed to {{ supportedGrowers }} supported growers, as well as 2200 [DeeNote: this number not is non-calc, dunno where to pull from] to the Mariannhill Co-Op Gardens. This seedling sale figure equates to an approximated planted area of {{ plantedArea }} m<span>2</span>, which if all are successfully grown represent an estimated yield  of approximately {{ cropYield }}T, which at retail value is about R{{ cropValue }} 000.  </p>
+                            <h4>Mentor Visits [no actual data yet]</h4>
+                            <p>45 mentor visits were conducted in July 2018 to 32 Growers.  Most mentor visits were conducted to capture crops in the ground (General Mentorship), to deliver seedlings, or collect produce/seed for sale.  </p>
+                        </v-flex>                              
+                    </v-card-text>
+                </v-card>             
+            </v-flex>
         </v-container>
     </v-layout>
 </template>
@@ -18,40 +25,40 @@
 import moment from "moment";
 
 export default {
-    computed: {
-        date(){
-            return moment(this.$store.getters.reportMonth).format("MMMM YYYY")
-        },
-        seedlingSum(){
-            return this.$store.getters.seedlingSum
-        },
-        salesForm() {
-            salesFormView = this.$store.getters.salesForm
-            console.log('​------------------------------------------');
-            console.log('​salesForm -> salesFormView', salesFormView);
-            console.log('​------------------------------------------');
-            
-            return salesFormView
-        },
-        supportedGrowers(){
-            return this.$store.getters.supportedGrowersCount
-        },
-        plantedArea() {
-            return this.$store.getters.plantedArea
-        },
-        cropYield() {
-            return this.$store.getters.cropYield
-        },
-        cropValue() {
-            return this.$store.getters.cropValue
-        }
+  computed: {
+    date() {
+      return moment(this.$store.getters.reportMonth).format("MMMM YYYY");
+    },
+    seedlingSum() {
+      return this.$store.getters.seedlingSum;
+    },
+    salesForm() {
+      salesFormView = this.$store.getters.salesForm;
+      console.log("​------------------------------------------");
+      console.log("​salesForm -> salesFormView", salesFormView);
+      console.log("​------------------------------------------");
+
+      return salesFormView;
+    },
+    supportedGrowers() {
+      return this.$store.getters.supportedGrowersCount;
+    },
+    plantedArea() {
+      return this.$store.getters.plantedArea;
+    },
+    cropYield() {
+      return this.$store.getters.cropYield;
+    },
+    cropValue() {
+      return this.$store.getters.cropValue;
     }
-}
+  }
+};
 </script>
 <style scoped>
-span{
-    vertical-align:super;
-    font-size:50%;
+span {
+  vertical-align: super;
+  font-size: 50%;
 }
 </style>
 
