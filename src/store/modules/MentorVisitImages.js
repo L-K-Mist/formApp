cb4df0e1c380250510602928b7e4e60b6325c450
Dylan <dylan@vandenbosch.co.za>
2018-09-03 12:10:56 +0200

    looks right

--- src/store/modules/MentorVisitImages.js
+++ src/store/modules/MentorVisitImages.js
@@ -6,9 +6,9 @@ const state = {
     photoReport: null
 }
 const getters = {
-  photoReport(state) {
-      return state.photoReport
-  }
+    photoReport(state) {
+        return state.photoReport
+    }
 };
 const actions = {
     processImageIndex({
@@ -36,45 +36,45 @@ const actions = {
         rootState.SeedlingSales.reportMonth = "2018-07"; // Hardcoded for now to avoid re-setting each time during dev-cycles.
 
 
-         // let result = appointments.map(a => ({...patients.find(p => a.patientId === p.patientId), ...a}));
+        // let result = appointments.map(a => ({...patients.find(p => a.patientId === p.patientId), ...a}));
         var mentorVisits = await db.get(rootState.SeedlingSales.reportMonth + "MentorVisits")
         var mentorPhotos = await db.get(rootState.SeedlingSales.reportMonth + "MentorPhotos")
 
-        
-        
+
+
         // TODO SOLUTION: Think I need a forEach first bacause it's not the whole array I'm iterating  but each photos array in photoVisits, that needs to find it's photo.
-            
-                
-                
- 
+
+
+
+
         var photoVisits = mentorVisits.mentorVisits // visits with all three photos (note: count starts at [0])
-        .filter(row => (row.photos[0] !== "No Image" && row.photos[1] !== "No Image" && row.photos[2] !== "No Image"))
-        
+            .filter(row => (row.photos[0] !== "No Image" && row.photos[1] !== "No Image" && row.photos[2] !== "No Image"))
+
         console.log('​-------------------------');
         console.log('​photoVisits', photoVisits);
         console.log('​-------------------------');
-         // let result = appointments.map(a => ({...patients.find(p => a.patientId === p.patientId), ...a}));
+        // let result = appointments.map(a => ({...patients.find(p => a.patientId === p.patientId), ...a}));
 
-         console.log(mentorPhotos.fsImages)
+        console.log(mentorPhotos.fsImages)
         var photoReport = []
-        photoVisits.forEach(function(row){
+        photoVisits.forEach(function (row) {
             var combo = row.photos.map(visitPhoto => ({
                 ...mentorPhotos.fsImages.find(photoRow => visitPhoto.name == photoRow.name)
             }))
-            console.log('​-------------');
-            console.log('​combo', combo);
-            console.log('​-------------');
-            var comboRow = { 
-                date: row.date, 
-                memberId: row.memberId, 
-                gps: row.gps, 
+            // console.log('​-------------');
+            // console.log('​combo', combo);
+            // console.log('​-------------');
+            var comboRow = {
+                date: row.date,
+                memberId: row.memberId,
+                gps: row.gps,
                 gardenName: row.gardenName,
-                 name: row.name, 
-                 nationalId: row.nationalId, 
-                 farmingActivity: row.farmingActivity, 
-                 memberArea: row.memberArea, 
-                 photos: combo
-                 };
+                name: row.name,
+                nationalId: row.nationalId,
+                farmingActivity: row.farmingActivity,
+                memberArea: row.memberArea,
+                photos: combo
+            };
             photoReport.push(comboRow)
         })
         photoReport.sort(function (obj1, obj2) {
@@ -85,8 +85,8 @@ const actions = {
         console.log("​-------------------------");
         console.log('photoReport', state.photoReport);
         console.log('​-------------------------');
-    }        
-}    
+    }
+}
 export default {
     state,
     getters,
