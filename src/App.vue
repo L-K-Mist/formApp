69b726f753d2e755b1619dea6187ed8b2918cfad
Dylan <dylan@vandenbosch.co.za>
2018-09-11 09:14:33 +0200

    Manual text inputs for farmers: Very basically done

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,7 @@
           :key="item.title"
           @click.stop="$router.push({
         path: item.path
-    })" 
-         
+    })"        
         >
           <v-list-tile-action>
             <v-icon v-html="item.icon"></v-icon>
@@ -36,15 +35,12 @@
       :clipped-left="clipped"
     >
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
-      
     </v-toolbar>
     <v-content>
       <router-view transition="slide-x-reverse-transition"/>
     </v-content>
-   
     <v-footer class="not-print" :fixed="fixed" app>
       <span>&copy; 2018</span>
     </v-footer>
